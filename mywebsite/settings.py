f2a42c9255f08f72c0e997693422ad255c2b0b5d
iris <iriskoca@gmail.com>
2025-01-31 11:48:16 +0300

    Django code change 4

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-#g4=b-nw+fn_vv$guhspr$ks5!7!*_(lo*@_04#d^l_yd4r6p0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.iristo.tech', 'iristo.tech']
+ALLOWED_HOSTS = ['*']
 
 SECURE_SSL_REDIRECT = False
 
